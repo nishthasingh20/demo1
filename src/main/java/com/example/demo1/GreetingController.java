a804d33b3b6b39a19b71b8ba19d3a8732cc449b1
nishthasingh20 <singhnishtha206@gmail.com>
2025-07-13 12:28:29 +0530

    message shown on backend

--- src/main/java/com/example/demo1/GreetingController.java
+++ src/main/java/com/example/demo1/GreetingController.java
@@ -1,27 +1,49 @@
 package com.example.demo1;
 
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.util.HtmlUtils;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import com.example.demo1.entity.ChatMessage;
 
 @Controller
 public class GreetingController {
     private static final Logger logger = LoggerFactory.getLogger(GreetingController.class);
 
-    @MessageMapping("/hello")
-    @SendTo("/topic/greetings")
-    public Greeting greeting(HelloMessage message) {
-        logger.info("Received message from: {}", message.getName());
-        try {
-            Thread.sleep(1000); // simulated delay -> 1s
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate;
+
+    @MessageMapping("/chat/{roomId}")
+    @SendTo("/topic/chat/{roomId}")
+    public ChatMessage greeting(@DestinationVariable String roomId, ChatMessage message) {
+        logger.info("Received message in room '{}': {} - {}", roomId, message.getSender(), message.getContent());
+//        try {
+//            Thread.sleep(1000); // simulated delay -> 1s
+//        } catch (InterruptedException e) {
+//            Thread.currentThread().interrupt();
+//        }
+//        String response = "Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!";
+//        logger.info("Sending response: {}", response);
+//        return new Greeting(response);
+        return message;
+    }
+
+    @MessageMapping("/chatroom/{email}")
+    public void privateChat(@DestinationVariable String email, ChatMessage message) {
+        logger.info("Received private message for '{}': {} - {}", email, message.getSender(), message.getContent());
+
+        // Send message to recipient
+        messagingTemplate.convertAndSend("/topic/chatroom/" + email, message);
+
+        // Send message to sender as well (if different from recipient)
+        String sender = message.getSender();
+        if (!sender.equals(email)) {
+            logger.info("Also sending message to sender: {}", sender);
+            messagingTemplate.convertAndSend("/topic/chatroom/" + sender, message);
         }
-        String response = "Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!";
-        logger.info("Sending response: {}", response);
-        return new Greeting(response);
     }
 }
